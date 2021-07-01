634520fa1d9b780cc982e5d291b0c1ffdabd7504
Ainouz <ainoumi@ad.vzwcorp.com>
2021-07-01 15:57:46 -0500

    adding some js files

---
+++ user.test.js
@@ -0,0 +1,21 @@
+const User = require('./user')
+
+describe('user class', () => {
+            const testuser = new User(12234, 'mike', 17, 'mike@gmail.com')
+
+            test('has name', () => {
+                expect().toBe()
+            })
+
+            test('', () => {
+                expect().toBe()
+            })
+
+            test('', () => {
+                expect().toEqual()
+            })
+
+            test('', () => {
+                    expect().toEqual()
+                }
+            })
